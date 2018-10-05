68c3e7e070b0fdc16266ae19bde91dedc4b2d079
Phil Hindle <phil.hindle@gmail.com>
2018-10-05 14:04:56 +0100

    Remove errorlens.sayHello command

--- src/extension.ts
+++ src/extension.ts
@@ -91,7 +91,7 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
 
-    
+
     /**
      * todo
      *
@@ -331,19 +331,6 @@ export function activate(context: vscode.ExtensionContext) {
         // }
         _statusBarItem.show();
     }
-
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('errorlens.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello ErrorLens World!');
-    });
-
-    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
